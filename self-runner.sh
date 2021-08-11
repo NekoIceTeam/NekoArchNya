5ef10eee41b7e45261c598f9fe71c441ffb371fd
NekoIceCream <nekoicecream@outlook.co.id>
2021-08-11 20:35:13 +0700

    Update build.sh, workflow, etc

--- self-runner.sh
+++ self-runner.sh
@@ -8,7 +8,7 @@ echo "Install Self-Runner!"
 sleep 2
 echo ""
 read -p "The Url repository ===>" url
-read -p "The Token ===>" token
+read -p "The Token ===> " token
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.280.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.280.1/actions-runner-linux-x64-2.280.1.tar.gz
 echo "24a8857b7f124f7c1852b030686a2f2205ec5d59ed7e2c0635a2c321d2b9fde6  actions-runner-linux-x64-2.280.1.tar.gz" | shasum -a 256 -c

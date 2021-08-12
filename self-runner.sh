b7604bce9aa6a9f1ad0db089689fe9a3a39162fc
NekoIceCream <nekoicecream@outlook.co.id>
2021-08-12 07:52:49 +0700

    Update Workflow

--- self-runner.sh
+++ self-runner.sh
@@ -7,7 +7,7 @@ clear
 echo "Install Self-Runner!"
 sleep 2
 echo ""
-read -p "The Url repository ===>" url
+read -p "The Url repository ===> " url
 read -p "The Token ===> " token
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.280.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.280.1/actions-runner-linux-x64-2.280.1.tar.gz
